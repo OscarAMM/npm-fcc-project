2f5a1be5d68f14cd81ec7b92851596c91cef4325
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-02-23 08:30:05 -0600

    FCC npm project

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); 
+
+app.use(express.static('public'));
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/api/hello', function(req, res){
+    res.json({
+        greetings: "Hello"
+    });
+});
+
+const port = 3000;
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
